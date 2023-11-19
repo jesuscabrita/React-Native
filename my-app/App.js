9c075a363688fd0621f013f5d88a0d80b88530e9
jesuscabrita <jesusarnaldo115@gmail.com>
2023-11-19 11:29:20 -0300

    front: inicio

---
+++ my-app/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hola Mundo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto:{
+    color:'white',
+    fontSize:'25px'
+  }
+});
